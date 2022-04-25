56d34a99320039c4a73c41d7bfb3fa3dddaf3631
ATJ12 <62201231+ATJ12@users.noreply.github.com>
2022-04-25 22:36:30 +0100

    pdf_chart

--- src/controllers/ChartFxmlController.java
+++ src/controllers/ChartFxmlController.java
@@ -30,14 +30,37 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+ 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.GrayColor;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Desktop;
+import java.text.SimpleDateFormat;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.WritableImage;
+import javax.imageio.ImageIO;
 /**
  * FXML Controller class
  *
  * @author Taha
  */
 public class ChartFxmlController implements Initializable {
-
+public static int numeroPDF = 0;
+    Document doc = new Document();
     /**
      * Initializes the controller class.
      */
@@ -75,6 +98,37 @@ public class ChartFxmlController implements Initializable {
 
   stage.show();
 
+    }
+
+    @FXML
+    private void pdf_ch(MouseEvent event) {
+                 numeroPDF = numeroPDF + 1;
+        String nom = "Graph statistique " + numeroPDF + ".pdf";
+        try {
+            SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat Heure = new SimpleDateFormat("hh:mm:ss");
+            //Font f = new Font(FontFamily.HELVETICA, 13, Font.NORMAL, GrayColor.GRAYWHITE);
+
+            WritableImage wimg = pieChart.snapshot(new SnapshotParameters(), null);
+            File file = new File("ChartSnapshot.png");
+            ImageIO.write(SwingFXUtils.fromFXImage(wimg, null), "png", file);
+
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(System.getProperty("user.home") + "\\Desktop\\" + nom));
+            doc.open();
+//            Image img = Image.getInstance("C:\\xampp\\htdocs\\TunisiaBonPlan2-Integration\\web\\uploads\\images\\Sanstitre.png");
+            Image img = Image.getInstance("ChartSnapshot.png");
+            doc.add(img);
+            doc.close();
+            Desktop.getDesktop().open(new File(System.getProperty("user.home") + "\\Desktop\\" + nom));
+            writer.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Error PDF");
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getMessage());
+
+        }
     }
     }   
 
